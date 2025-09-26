b6c471828cad9e5851361258a27d430af1169dc8
sachinsisondiya <Sachinsisondiya912@gmail.com>
2025-09-26 23:24:09 +0530

    header and footer complete

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,14 @@
 export default function Footer(){
-  return <p> i am footer</p>
+  return(
+  <footer>
+    <p>Ⓒ 2025 buzzy India Pvt Ltd. All rights reserved</p>
+    <div className="social-icons-div">
+      <img className="social-icon" src="../src/assets/facebook.png" alt="Facebook logo" />
+      <img className="social-icon" src="../src/assets/linkedin.png" alt="LinkedIn logo" />
+      <img className="social-icon" src="../src/assets/twitter.png" alt="Twitter logo" />
+      <img className="social-icon" src="../src/assets/instagram.png" alt="Instagram logo" />
+    </div>
+
+  </footer>
+  )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,28 @@
 import {Link , NavLink} from "react-router-dom"
 export default function Header(){
-  return <p>I am header</p>
+  return (
+       <header className="header">
+        <Link to="/"
+        className="home-link"
+        >#bu<strong>ZZ</strong>y<img
+        className="home-logo"
+         src="../src/assets/bus.svg" alt="red color location point bar , that include a bus" /></Link>
+
+         <nav>
+          <NavLink
+          className="nav-link"
+          to="/user">User</NavLink>
+
+          <NavLink className="nav-link" to="/driver/Travel">Driver</NavLink>
+
+          <NavLink className="nav-link" to="/admin">Admin</NavLink>
+
+          
+         </nav>
+
+  
+
+
+       </header>
+  )
 }
