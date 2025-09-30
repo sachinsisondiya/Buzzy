57795ca5da5952e47f068241bf98930b566cd6a3
sachinsisondiya <Sachinsisondiya912@gmail.com>
2025-09-30 22:35:05 +0530

    create login page

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -1,4 +1,44 @@
 import react from "react"
 export default function Admin(){
-  return <h1>Admin Page</h1>
+  return (
+    <>
+    <h1>Enter the details of drivers</h1>
+    <form>
+     <label for="firstName">First Name</label>
+     <input
+     type="text"
+      placeholder="Satashi"
+      id="firstName"
+      required
+      />
+
+      <label for="lastName">Last Name</label>
+      <input
+      type="text"
+      placeholder="Nakamoto"
+      id="lastName"
+      required
+      />
+
+      <label for="email">Email</label>
+      <input
+      type="email"
+      placeholder="abc@gmail.com"
+      id="email"
+      required
+      />
+
+      <label for="password">Password</label>
+      <input
+       type="password"
+       placeholder="********"
+       id="password"
+       required
+      />
+
+
+    <button type="submit">Submit</button>
+    </form>
+    </>
+  )
 }
